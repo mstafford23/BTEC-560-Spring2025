a4729ed245453ca42a9ff8580f79cc5df07618a2
mstafford23 <mstafford@claflin.edu>
2025-01-28 10:13:47 -0500

    Add files via upload
    Descriptive Measures File

---
+++ Descriptive-Measures.r
@@ -0,0 +1,28 @@
+#Author:Madison,Date:1/28/25, Purpose:Descriptive measures functions
+
+#Calculate standard devaiation
+#Create Dummy dataset
+data <-c(1,2,3,4,5,6,7,8)
+data1 <-c("A","B","C")
+data2 <-c(1:1000)
+
+sd(data)
+#[1] 2.44949
+sd(data2)
+#[1] 288.8194
+
+#Calculate mean
+
+mean(data)
+#[1] 4.5
+
+#Calculate median
+median(data)
+
+#Calculate mode in R
+
+
+
+
+
+
