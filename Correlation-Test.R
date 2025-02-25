d83b53b3f429d28bfc96f40565409e8d030d08f6
mstafford23 <mstafford@claflin.edu>
2025-02-25 10:16:27 -0500

    Add files via upload

--- Correlation-Test.R
+++ Correlation-Test.R
@@ -6,6 +6,22 @@ library("ggpubr")
 my_data <- mtcars
 
 #Create a correlation hypothesis
-Weight of the car is inversely proportional to the miles per gallon travelled by car
+#Weight of the car is inversely proportional to the miles per gallon travelled by car
 
-ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+Plot <- ggscatter(my_data, main="Comparison of cars weight and mpg", x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+Plot + theme(axis.text.x= element_text(size=50), axis.text.y= element_text(size=50))
+
+res <- cor.test(my_data$wt, my_data$mpg, method = "pearson")
+res
+
+Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
